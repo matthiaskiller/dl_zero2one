21156dc1d52aa26359e11ef2840b040928cf42e2
matthiaskiller <matthias_k@gmx.net>
2024-06-14 00:16:37 +0200

    add cifar10_pytorch lighning notebook

--- dl_zero2one/MyPytorchModel.py
+++ dl_zero2one/MyPytorchModel.py
@@ -2,8 +2,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, random_split
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader, random_split, SubsetRandomSampler
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
@@ -14,66 +13,40 @@ class MyPytorchModel(pl.LightningModule):
     def __init__(self, hparams, input_size=3 * 32 * 32, num_classes=10):
         super().__init__()
 
-        # set hyperparams
-        self.hparams = hparams
+        # Set hyperparams
+        self.save_hyperparameters(hparams)        
         self.model = None
 
-        ########################################################################
-        # TODO: Initialize your model!                                         #
-        ########################################################################
-
-        if self.hparams["activation"] == "ReLU":
-            activation = nn.ReLU()
-        elif self.hparams["activation"] == "LeakyReLU":
-            activation = nn.LeakyReLU()
-        else:
-            activation = nn.Tanh()
-
+        # Initialize your model
+        activation = getattr(nn, self.hparams["activation"])()
         self.model = nn.Sequential(
             nn.Linear(input_size, self.hparams["n_hidden"]),
             activation,
             nn.Linear(self.hparams["n_hidden"], num_classes)
         )
 
-        ########################################################################
-        #                           END OF YOUR CODE                           #
-        ########################################################################
-
     def forward(self, x):
-
         # x.shape = [batch_size, 3, 32, 32] -> flatten the image first
         x = x.view(x.shape[0], -1)
-
         # feed x into model!
         x = self.model(x)
-
         return x
 
     def general_step(self, batch, batch_idx, mode):
         images, targets = batch
-
         # forward pass
         out = self.forward(images)
-
         # loss
         loss = F.cross_entropy(out, targets)
-
         preds = out.argmax(axis=1)
         n_correct = (targets == preds).sum()
+        self.log(f'{mode}_loss', loss, prog_bar=True)
+        self.log(f'{mode}_accuracy', n_correct / len(targets), prog_bar=True)
         return loss, n_correct
-
-    def general_end(self, outputs, mode):
-        # average over all batches aggregated during one epoch
-        avg_loss = torch.stack([x[mode + '_loss'] for x in outputs]).mean()
-        total_correct = torch.stack(
-            [x[mode + '_n_correct'] for x in outputs]).sum().cpu().numpy()
-        acc = total_correct / len(self.sampler[mode])
-        return avg_loss, acc
 
     def training_step(self, batch, batch_idx):
         loss, n_correct = self.general_step(batch, batch_idx, "train")
-        tensorboard_logs = {'loss': loss}
-        return {'loss': loss, 'train_n_correct': n_correct, 'log': tensorboard_logs}
+        return {'loss': loss, 'train_n_correct': n_correct}
 
     def validation_step(self, batch, batch_idx):
         loss, n_correct = self.general_step(batch, batch_idx, "val")
@@ -83,32 +56,26 @@ class MyPytorchModel(pl.LightningModule):
         loss, n_correct = self.general_step(batch, batch_idx, "test")
         return {'test_loss': loss, 'test_n_correct': n_correct}
 
-    def validation_end(self, outputs):
-        avg_loss, acc = self.general_end(outputs, "val")
-        # print("Val-Acc={}".format(acc))
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'val_loss': avg_loss, 'val_acc': acc, 'log': tensorboard_logs}
+    def early_stopping(self, val_loss):
+        return {'val_loss': val_loss}
 
     def prepare_data(self):
-
-        # create dataset
-        CIFAR_ROOT = "/content/datasets/cifar10"
-        my_transform = None
+        CIFAR_ROOT = "../datasets/cifar10"
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
-        ########################################################################
-        # TODO: Define your transforms (convert to tensors, normalize).        #
-        # If you want, you can also perform data augmentation!                 #
-        ########################################################################
 
-        my_transform = transforms.Compose(
-            [transforms.RandomHorizontalFlip(p=0.5), transforms.ToTensor(), transforms.Normalize(mean, std)])
+        # Define your transforms (convert to tensors, normalize)
+        my_transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
 
-        ########################################################################
-        #                           END OF YOUR CODE                           #
-        ########################################################################
-        train_val_transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize(mean, std)])
+        train_val_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
         cifar_complete_augmented = torchvision.datasets.ImageFolder(
             root=CIFAR_ROOT, transform=my_transform)
         cifar_complete_train_val = torchvision.datasets.ImageFolder(
@@ -118,17 +85,14 @@ class MyPytorchModel(pl.LightningModule):
         num_train, num_val = int(N*0.6), int(N*0.2)
         np.random.seed(0)
         indices = np.random.permutation(N)
-        train_idx, val_idx, test_idx = indices[:num_train], indices[num_train:num_train +
-                                                                    num_val], indices[num_train+num_val:]
+        train_idx, val_idx, test_idx = indices[:num_train], indices[num_train:num_train + num_val], indices[num_train+num_val:]
         train_sampler = SubsetRandomSampler(train_idx)
         val_sampler = SubsetRandomSampler(val_idx)
         test_sampler = SubsetRandomSampler(test_idx)
-        self.sampler = {"train": train_sampler,
-                        "val": val_sampler, "test": test_sampler}
+        self.sampler = {"train": train_sampler, "val": val_sampler, "test": test_sampler}
 
-        # assign to use in dataloaders
-        self.dataset = {}
-        self.dataset["train"], self.dataset["val"], self.dataset["test"] = cifar_complete_augmented, cifar_complete_train_val, cifar_complete_train_val
+        # Assign to use in dataloaders
+        self.dataset = {"train": cifar_complete_augmented, "val": cifar_complete_train_val, "test": cifar_complete_train_val}
 
     def train_dataloader(self):
         return DataLoader(self.dataset["train"], batch_size=self.hparams["batch_size"], sampler=self.sampler["train"], num_workers=self.hparams["num_workers"])
@@ -140,19 +104,9 @@ class MyPytorchModel(pl.LightningModule):
         return DataLoader(self.dataset["test"], batch_size=self.hparams["batch_size"], sampler=self.sampler["test"], num_workers=self.hparams["num_workers"])
 
     def configure_optimizers(self):
-
-        optim = None
-        ########################################################################
-        # TODO: Define your optimizer.                                         #
-        ########################################################################
-
-        optim = torch.optim.Adam(
-            self.model.parameters(), lr=self.hparams["learning_rate"], weight_decay=self.hparams["weight_decay"])
-
-        ########################################################################
-        #                           END OF YOUR CODE                           #
-        ########################################################################
-        return optim
+        return torch.optim.Adam(
+            self.model.parameters(), lr=self.hparams["learning_rate"], weight_decay=self.hparams["weight_decay"]
+        )
 
     def getTestAcc(self, loader=None):
         self.model.eval()
